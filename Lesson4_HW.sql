497db07ccb20cea2733f01d5588794bdaecddd30
Dmitri <shizimaniak@gmail.com>
2022-10-06 10:31:47 +0300

    Оказать помощь

--- Lesson4_HW.sql
+++ Lesson4_HW.sql
@@ -58,6 +58,8 @@ DROP VIEW BMW;
 
 DROP VIEW other;
 
+--использование подзапросов
+
 SELECT
 	a.mark,
 	COUNT(a.mark),
@@ -95,3 +97,4 @@ SELECT * FROM test_a
 LEFT JOIN test_b
 ON test_b.id = test_a.id
 WHERE test_b.id IS NULL;
+
